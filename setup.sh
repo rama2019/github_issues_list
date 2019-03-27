f485594bc86e7ca6a05bef1b932cd1b480e9fec4
rama2019 <fecnotify12@gmail.com>
2019-03-27 21:51:04 +0530

    minor fix in setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-virtualenv venv # install virtual env
+virtualenv -p python3 venv # install virtual env
 source venv/bin/activate # activate virtual env
 pip install -r requirements.txt # installing requirements from requirements.txt
 export FLASK_APP=app.py # export app.py 
