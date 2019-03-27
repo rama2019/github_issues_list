5679f9c9bf628419bb42ba8ea64ead713616f186
rama2019 <fecnotify12@gmail.com>
2019-03-27 10:28:42 +0530

    Bug Fixing

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import datetime
 import json
-
+from dateutil import parser
 import requests
 from flask import Flask, render_template, request
 
@@ -32,7 +32,7 @@ def home():
     # validate url before retrieving any data from github
 
     response_for_all_open_issues = requests.get(issues_url)
-
+    print (response_for_all_open_issues.status_code)
     if response_for_all_open_issues.status_code != 200:
         return render_template(
             template_name, 
@@ -42,49 +42,64 @@ def home():
         )
 
     # retrieve all open issues 
-    all_open_issues_count = get_all_open_issues(issues_url)
+    all_issues = get_issues_recursively(issues_url)
 
     # retrieve issues opened in last 24hrs
-    open_issues_count_in_last_24_hrs = get_open_issues_opened_in_last_24_hrs(issues_url)
+    open_issues_count_in_last_24_hrs = 0
 
     # Number of open issues that were opened more than 24 hours ago but less than 7 days ago
-    open_issues_count_in_last_7_days = get_open_issues_opened_in_last_7_days(issues_url) - open_issues_count_in_last_24_hrs
+    open_issues_count_in_last_7_days = 0
     
+    all_open_issues_count = 0
+
+    last_24_hours_datetime = datetime.datetime.now()- datetime.timedelta(days=1)
+    last_7_days_datetime = datetime.datetime.now()- datetime.timedelta(days=7)
+
+    
+    for item in all_issues:
+        if 'pull_request' not in item :
+            all_open_issues_count += 1
+            created_at = parser.parse(item['created_at'])
+            created_at = created_at.replace(tzinfo=None)
+
+            if created_at >= last_24_hours_datetime :
+                open_issues_count_in_last_24_hrs += 1
+            elif last_7_days_datetime <= created_at < last_24_hours_datetime :
+                open_issues_count_in_last_7_days += 1
+
+
     return render_template(template_name, context={
         'all_issues': all_open_issues_count,
         'opened_in_last_24_hrs': open_issues_count_in_last_24_hrs,
         'opened_in_last_7_days':open_issues_count_in_last_7_days,
-        'opened_before_7_days': all_open_issues_count-open_issues_count_in_last_7_days,
+        'opened_before_7_days': all_open_issues_count-open_issues_count_in_last_7_days-open_issues_count_in_last_24_hrs,
         'user_repo_url': user_given_repo_url
     })
 
 def format_url(repo_url):
     return "/".join(repo_url.split('/')[-2:])
 
-def get_all_open_issues(repo_url):
+def get_issues_recursively(repo_url):
     """
-        Return the all open issues count for the given repo ( Public )
+        utility function to retrive issues 
+        ( per_page doesn't work for more than 100 in a single network call. 
+        it returns max 100 at a time 
+        ) 
     """
-    response_for_all_open_issues = requests.get(repo_url)
-    jsonify_content = json.loads(response_for_all_open_issues.content)
-    return len(jsonify_content)
+    page  = 1
+    page_size = 100
 
-def get_open_issues_opened_in_last_24_hrs(repo_url):
-    """
-        Return the all open issues count opened in last 24Hrs for the given repo ( Public )
-    """
-    last_24_hours_datetime = datetime.datetime.now()- datetime.timedelta(days=1)
-    repo_url = repo_url + "&since={}".format(last_24_hours_datetime)
-    response_for_all_open_issues = requests.get(repo_url)
-    jsonify_content = json.loads(response_for_all_open_issues.content)
-    return len(jsonify_content)
+    issues_fetched = 100
+    all_issues = []
+    repo_url = repo_url+ "&page={}&per_page=100"
+    while issues_fetched == page_size:
+        new_repo_url = repo_url.format(page)
+        response = requests.get(new_repo_url)
+        jsonify_content = json.loads(response.content)
+        all_issues.extend(jsonify_content)
+        issues_fetched = len(jsonify_content)
+        page += 1
 
-def get_open_issues_opened_in_last_7_days(repo_url):
-    """
-        Return the all open issues count opened in last 7 days for the given repo ( Public )
-    """
-    last_7_days_datetime = datetime.datetime.now()- datetime.timedelta(days=7)
-    repo_url = repo_url + "&since={}".format(last_7_days_datetime)
-    response_for_all_open_issues = requests.get(repo_url)
-    jsonify_content = json.loads(response_for_all_open_issues.content)
-    return len(jsonify_content)
+    return all_issues
+
+
